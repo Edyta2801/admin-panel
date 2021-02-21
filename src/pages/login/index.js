975afb337c5f5fb4b17b65cd1d4cad6e6d667360
Edyta2801 <edyta.szarowska@interia.pl>
2021-02-21 11:51:57 +0100

    e-mail and passwoed field

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import {Flex, Stack, Box, Heading} from '@chakra-ui/react';
+import { Flex, Stack, Box, Heading } from '@chakra-ui/react';
+import LoginForm from '../../components/LoginForm';
 
-function LoginForm(){
-    return(
-        <Flex minHeight="100vh" align="center" justify="center">
-        <Stack spacing={8} mx='auto' width='full' py={12} px={6} >
-            <Heading fontSize="3xl" textAlign="center">
-                Sign up
-            </Heading>
-            <Box rounded="lg" bg="grey" boxShadow="lg" p={{base:4, md:8}}>
-{/* <LoginForm/> */}
-            </Box>
-        </Stack>
-
-        </Flex>
-
-    );
+function LoginPage() {
+  return (
+    <Flex minHeight="100vh" align="center" justify="center" bg="gray.200">
+      <Stack spacing={8} mx="auto" width="full" py={12} px={6}>
+        <Heading fontSize="3xl" textAlign="center">
+          Sign up
+        </Heading>
+        <Box rounded="lg" bg="white" boxShadow="lg" p={{ base: 4, md: 8 }}>
+          <LoginForm />
+        </Box>
+      </Stack>
+    </Flex>
+  );
 }
-export default LoginForm
+
+export default LoginPage;
