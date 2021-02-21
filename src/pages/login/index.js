02398e0f8f054ef347fedab52a971a3cead04e9b
Edyta2801 <edyta.szarowska@interia.pl>
2021-02-21 11:28:23 +0100

    npm install --save react-hook-form

---
+++ src/pages/login/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Flex, Stack, Box, Heading} from '@chakra-ui/react';
+
+function LoginForm(){
+    return(
+        <Flex minHeight="100vh" align="center" justify="center">
+        <Stack spacing={8} mx='auto' width='full' py={12} px={6} >
+            <Heading fontSize="3xl" textAlign="center">
+                Sign up
+            </Heading>
+            <Box rounded="lg" bg="grey" boxShadow="lg" p={{base:4, md:8}}>
+{/* <LoginForm/> */}
+            </Box>
+        </Stack>
+
+        </Flex>
+
+    );
+}
+export default LoginForm
